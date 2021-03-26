998623a6da09d2813ef26bae2792b987b928d1ee
Caio Fuccio <caio.fuccio@gmail.com>
2021-03-26 15:12:30 -0300

    added global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --header-background: #f8f8f8;
+    --content-background: #D4d4d4;
+    --footer-background: #6b6b6b;
+
+    --card-background: #F0F0F0;
+    --card-border: #353535
+
+    --black: #000;
+    --white: #fff;
+    --dark-gray: #333;
+    --orange: #f29d16;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+
+  html {
+    @media (max-width: 1080px){
+      font-size: 93.75%;
+    }
+
+    @media (max-width:720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    height: 100vh;
+    background: linear-gradient(to bottom, var(--header-background) 0, var(--header-background) 25%,
+    var(--content-background) 25%, var(--content-background) 94%,
+    var(--footer-background) 94%, var(--footer-background) 100%);
+  }
+`;
